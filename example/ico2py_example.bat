81078cc7c21dffae99a47d40cb16ab2e9c50f7e0
d10n <david@bitinvert.com>
2012-12-12 01:58:17 -0500

    Example

---
+++ example/ico2py_example.bat
@@ -0,0 +1,2 @@
+set PYTHON=C:\Python27\python.exe
+%PYTHON% ..\ico2py.py nano_16.png nano_48.png nano_90.png nano_icon.py
---
+++ example/ico2py_example.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python2 ../ico2py.py nano_16.png nano_48.png nano_90.png nano_icon.py
+
